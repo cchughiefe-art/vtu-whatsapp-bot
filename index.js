6fdf10637f94ccf54a6779705842e627d43647b3
courage <cchughiefe@gmail.com>
2026-03-29 13:19:17 +0100

    just continue

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is online! 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`Keep-alive server listening on port ${port}`);
+});
+
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const { MongoClient } = require('mongodb');
 const P = require('pino');
